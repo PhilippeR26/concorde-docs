4e37d73cb30214eff013c0689ed168e7cb9a9500
PhilippeR26 <philippe.rostan@yahoo.fr>
2024-09-17 08:35:49 +0200

    build: TU1 completed

--- Pages/Divers/contact.php
+++ Pages/Divers/contact.php
@@ -1,24 +1,37 @@
+<?php
+function debug_to_console($data)
+{
+	$output = $data;
+	if (is_array($output))
+		$output = implode(',', $output);
+	echo "<script>console.log('console: " . $output . "' );</script>";
+}
 
-<?php 
-	mail('concorde.docs@free.fr','Message provenant du site http://concorde.docs.free.fr',$_POST['Txt1'])
-?>
+$msg = wordwrap($_POST['Txt1'], 70);
+debug_to_console($msg);
+mail('concorde.docs@free.fr', 'Message provenant du site http://concorde.docs.free.fr', $msg)
+	?>
 
 
 <body bgcolor="#1E435E">
 
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<table border="1" align="center" cellpadding="20" cellspacing="0" bordercolor="#666666" bgcolor="#FFFFFF">
-<tr>
-<td align="center">
-<p><font size="2" face="Verdana, Arial, Helvetica, sans-serif"><strong>
-Votre message a bien &eacute;t&eacute; envoy&eacute;
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+	<table border="1" align="center" cellpadding="20" cellspacing="0" bordercolor="#666666" bgcolor="#FFFFFF">
+		<tr>
+			<td align="center">
+				<p>
+					<font size="2" face="Verdana, Arial, Helvetica, sans-serif"><strong>
+							Votre message a bien &eacute;t&eacute; envoy&eacute;
 
-</strong></font></p>
-<p><font size="2" face="Verdana, Arial, Helvetica, sans-serif">[ <a href="javascript:history.go(-1)">Retour</a> ]</font></p>
-</td>
-</tr>
-</table>
-
+						</strong></font>
+				</p>
+				<p>
+					<font size="2" face="Verdana, Arial, Helvetica, sans-serif">[ <a
+							href="javascript:history.go(-1)">Retour</a> ]</font>
+				</p>
+			</td>
+		</tr>
+	</table>
---
+++ Scripts/exploreButtons.js
@@ -0,0 +1,38 @@
+function padG(chiff, nb) {
+  let textpage = chiff.toString();
+  let textpad = "";
+  while (textpad.length < (nb - textpage.length)) { textpad = textpad + "0"; }
+  textpad = textpad + textpage;
+  return textpad;
+}
+function updatePage(root) { // mise à jour suite à appui bouton
+  document.getElementById('titreHaut').innerHTML = pageCur;
+  switch (pageCur) {
+    case 1:
+      //console.log('premiere');
+      document.getElementById('bouton_prem').style.visibility = 'hidden';
+      document.getElementById('bouton_prec').style.visibility = 'hidden';
+      document.getElementById('bouton_suiv').style.visibility = 'visible';
+      document.getElementById('bouton_der').style.visibility = 'visible';
+      break;
+    case pageMax:
+      //console.log('derniere');
+      document.getElementById('bouton_prem').style.visibility = 'visible';
+      document.getElementById('bouton_prec').style.visibility = 'visible';
+      document.getElementById('bouton_suiv').style.visibility = 'hidden';
+      document.getElementById('bouton_der').style.visibility = 'hidden';
+      break;
+    default:
+      //console.log('interm');
+      document.getElementById('bouton_prem').style.visibility = 'visible';
+      document.getElementById('bouton_prec').style.visibility = 'visible';
+      document.getElementById('bouton_suiv').style.visibility = 'visible';
+      document.getElementById('bouton_der').style.visibility = 'visible';
+      break;
+  }
+  imaG = padG(pageCur * 2 - 1, 3);
+  imaD = padG(pageCur * 2, 3);
+  document.getElementById('image1').src = root + imaG + ".jpg";
+  document.getElementById('image2').src = root + imaD + ".jpg";
+
+}
